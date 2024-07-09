823c28ecad2349d0af70a74d906e5439a47dc96c
Jasper Aikema <jasper.aikema@proserve.nl>
2024-07-09 15:09:16 +0200

    Add authenticate function

---
+++ contents/salt.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python3
+
+import requests
+import json
+import logging
+
+logger = logging.getLogger(__name__)
+logging.basicConfig(level=logging.INFO, format='%(levelname)s - %(message)s')
+
+class SaltApiNodeStepPlugin(object):
+
+    def __init__(self, endpoint=None, username=None, password=None, eauth='auto'):
+        self.endpoint = endpoint
+        self.username = username
+        self.password = password
+        self.eauth = eauth
+
+    def authenticate(self):
+        """
+        Authenticate with the Salt API and store the token
+        """
+
+        data = {
+            "username": self.username,
+            "password": self.password,
+            "eauth": self.eauth
+        }
+
+        headers = {
+            "Accept": "application/json",
+            "Content-Type": "application/json"
+        }
+
+        url = f"{self.endpoint}/login"
+
+        logger.info("Authenticating with salt-api endpoint: [%s]", url);
+
+        response = requests.post(url,
+                                headers=headers,
+                                data=json.dumps(data))
+
+        if response.status_code == 200:
+            try:
+                token = response.json()["return"][0]["token"]
+                return token
+            except NameError:
+                raise Exception(f"Error fetching token: ${response.text()}")
+            except Exception as e:
+                print("Error:", e)
+                raise Exception(f"Error fetching token: ${response.text()}")
+        elif response.status_code == 401:
+            return None
+        else:
+            raise Exception(f"Unexpected failure interacting with salt-api {response.text()}")
---
+++ contents/tests/test_authenticate.py
@@ -0,0 +1,68 @@
+import time
+import unittest
+import json
+from unittest import mock
+
+from salt import SaltApiNodeStepPlugin
+
+class TestSaltApiNodeStepPlugin(unittest.TestCase):
+
+    def setUp(self):
+
+        self.PARAM_ENDPOINT = "https://localhost"
+        self.PARAM_EAUTH = "pam"
+        self.PARAM_MINION_NAME = "minion"
+        self.PARAM_FUNCTION = "some.function"
+        self.PARAM_USER = "user"
+        self.PARAM_PASSWORD = "password&!@$*"
+        self.AUTH_TOKEN = "123qwe";
+        self.OUTPUT_JID = "20130213093536481553";
+        self.HOST_RESPONSE = "\"some response\"";
+
+        self.plugin = SaltApiNodeStepPlugin(self.PARAM_ENDPOINT, self.PARAM_USER, self.PARAM_PASSWORD, self.PARAM_EAUTH)
+
+    @mock.patch('requests.post')
+
+    def test_authenticate_with_ok_response_code(self, mock_post):
+        mock_post.return_value.status_code = 200
+        mock_post.return_value.json.return_value = {"return": [{
+            "token": self.AUTH_TOKEN,
+            "start": time.time(),
+            "expire": time.time()+(12*60*60),
+            "user": self.PARAM_USER,
+            "eauth": self.PARAM_EAUTH,
+            "perms": [
+                "grains.*",
+                "status.*",
+                "sys.*",
+                "test.*"
+            ]
+        }]}
+
+        result = self.plugin.authenticate()
+        self.assertEqual(result, self.AUTH_TOKEN)
+        mock_post.assert_called_once_with(
+            self.PARAM_ENDPOINT+'/login',
+            headers = {"Accept": "application/json", "Content-Type": "application/json"},
+            data=json.dumps({
+                "username": self.PARAM_USER,
+                "password": self.PARAM_PASSWORD,
+                "eauth": self.PARAM_EAUTH,
+            })
+        )
+
+    @mock.patch('requests.post')
+    def test_authenticate_failure(self, mock_post):
+        mock_post.return_value.status_code = 401
+        mock_post.return_value.json.return_value = {}
+
+        result = self.plugin.authenticate()
+        self.assertIsNone(result)
+
+    @mock.patch('requests.post')
+    def test_authentication_failure_on_internal_server_error(self, mock_post):
+        mock_post.return_value.status_code = 500
+        mock_post.return_value.json.return_value = {}
+
+        with self.assertRaises(Exception):
+            self.plugin.authenticate()
